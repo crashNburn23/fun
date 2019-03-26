30c4a146ec0b1da0ac07b2c9ce295762fb681447
crashNburn23 <darrell.lytle@gmail.com>
2019-03-26 14:52:14 +0100

    Update shellcodedump.sh

--- shellcodedump.sh
+++ shellcodedump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 name=${1?Error: Provide binary name!}
-
+echo -e "Below is your shellcode: \n"
 for i in $(objdump -d $name |grep "^ " |cut -f2); do echo -n '\x'$i; done; echo
+echo -e "\nTesting for null bytes... \n"
+for i in $(objdump -d $name |grep "^ " |cut -f2); do echo -n '\x'$i | grep "\x00"; done
